b18fafa8b1df396c7baab8385c53f1c65a572c38
Maxens Legoupil <legoupi231@campus.unicaen.fr>
2024-10-04 13:57:12 +0200

    fix : divers problème résolu, bon point de départ pour finir la liste déroulente

--- script/script.js
+++ script/script.js
@@ -1,83 +1,37 @@
 const information = document.getElementById("information");
 const codePostal = document.getElementById("cp");
 
-codePostal.addEventListener("keypress", (e)=>{
+codePostal.addEventListener("keypress", (e) => {
     if (e.target.type === "number" && !e.key.match(/^[0-9]+$/)) {
         e.preventDefault();
-      }
-    if (codePostal.value.length > 4) {   
-        codePostal.value = codePostal.value.slice(0,4);
     }
-}, false );
-
-/*let champsCodePostal = document.getElementById("cp")
-
-champsCodePostal.addEventListener("input",(e)=>{
-    if(champsCodePostal.value.length == 5 && champsCodePostal.value < "99139" ){
-        console.log(getDataCommune())
-        let reponseAPICommune = getDataCommune()
-        new inputCommune = document.createElement("input")
-        for(let i = 0; i < reponseAPICommune.length  )
+    if (codePostal.value.length > 4) {
+        codePostal.value = codePostal.value.slice(0, 4);
     }
-
-})
-
-async function getDataCommune() {
-    const url = "https://geo.api.gouv.fr/communes?codePostal="+champsCodePostal.value;
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-        return await response.json();
-    } catch (error) {
-      console.error(error.message);
+    if (codePostal.value.length == 4) {
+        codePostal.value = codePostal.value + e.key; 
+        getData().then((json) =>{
+        for (var i = 0; i < json.length; i++) {
+            console.log(json[i]);
+        }})
+        console.log("json");
+        e.preventDefault();
     }
-  }
-  */
-
-
-
-  //getDataCommune()
-
-
-/*
-  async function getData() {
-    const url = "https://geo.api.gouv.fr/communes?codePostal=50000";
-    try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json();
-      console.log(json[0]["nom"])
-      let machin = json[0]["nom"];
-      return machin;
-    } catch (error) {
-      console.error(error.message);
-    }
-  }
-  console.log(getData())
-
-
+}, false);
 
 async function getData() {
-    const url = "https://api.meteo-concept.com/api/ephemeride/0?token=75e38b3c4f84616e8c5d703a6a6271ffcdad018bac865662eeb3f8b5766bec42";
+    const url = "https://geo.api.gouv.fr/communes?codePostal=" + codePostal.value;
+    console.log(url)
     try {
-      const response = await fetch(url);
-      if (!response.ok) {
-        throw new Error(`Response status: ${response.status}`);
-      }
-  
-      const json = await response.json;
-      console.log(json);
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+
+        var json = await response.json();
+        let machin = json[0]["nom"];
+        return json;
     } catch (error) {
-      console.error(error.message);
+        console.error(error.message);
     }
-  }
-
-  console.log("test")
-  getData()*/
-
+}
