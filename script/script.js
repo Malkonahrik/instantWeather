f5783411811d45e95ed8ed80eb80ec0c39823eb4
Maxens Legoupil <legoupi231@campus.unicaen.fr>
2024-10-04 13:04:07 +0200

    fix : bug de l'expression

--- script/script.js
+++ script/script.js
@@ -2,10 +2,14 @@ const information = document.getElementById("information");
 const codePostal = document.getElementById("cp");
 
 codePostal.addEventListener("keypress", (e)=>{
+    if (e.target.type === "number" && !e.key.match(/^[0-9]+$/)) {
+        e.preventDefault();
+      }
     if (codePostal.value.length > 4) {   
         codePostal.value = codePostal.value.slice(0,4);
     }
-}, false )
+}, false );
+
 /*let champsCodePostal = document.getElementById("cp")
 
 champsCodePostal.addEventListener("input",(e)=>{
@@ -38,7 +42,7 @@ async function getDataCommune() {
   //getDataCommune()
 
 
-
+/*
   async function getData() {
     const url = "https://geo.api.gouv.fr/communes?codePostal=50000";
     try {
@@ -59,7 +63,7 @@ async function getDataCommune() {
 
 
 
-/*async function getData() {
+async function getData() {
     const url = "https://api.meteo-concept.com/api/ephemeride/0?token=75e38b3c4f84616e8c5d703a6a6271ffcdad018bac865662eeb3f8b5766bec42";
     try {
       const response = await fetch(url);
