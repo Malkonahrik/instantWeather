e7d7cf1c131ebc12fe433836392b7ed5976d311b
Alexis LOYANT <alexis.loyant@unicaen.fr>
2024-10-08 16:17:27 +0200

    css pour input + select

---
+++ script/script.js
@@ -0,0 +1,47 @@
+const information = document.getElementById("information");
+const codePostal = document.getElementById("cp");
+const erreur = document.getElementById("errorMessage");
+const communeSelect = document.getElementById("communeSelect");
+
+codePostal.addEventListener("input", (e) => {
+    if (e.target.type === "number" && !e.key.match(/^[0-9]+$/)) {
+        e.preventDefault();
+    }
+    if (codePostal.value.length > 5) {
+        codePostal.value = codePostal.value.slice(0, 5);
+    }
+    if (codePostal.value.length == 5) {
+        getData().then((json) => {
+            if (json == -1) {
+                erreur.classList.remove("cache");
+            } else {
+                erreur.classList.add("cache");
+                json.forEach(element => {
+                    const option = document.createElement('option');
+                    option.value = element["nom"].toLowerCase().replace(/\s+/g, '-');
+                    option.textContent = element["nom"];
+                    communeSelect.appendChild(option);
+                });
+                communeSelect.classList.remove("cache");
+            }
+        });
+    } else {
+        communeSelect.replaceChildren();
+        communeSelect.classList.add("cache");
+    }
+}, false);
+
+async function getData() {
+    const url = "https://geo.api.gouv.fr/communes?codePostal=" + codePostal.value;
+    try {
+        const response = await fetch(url);
+        if (!response.ok) {
+            throw new Error(`Response status: ${response.status}`);
+        }
+        var json = await response.json();
+        let machin = json[0]["nom"];
+        return json;
+    } catch (error) {
+        return -1;
+    }
+}
