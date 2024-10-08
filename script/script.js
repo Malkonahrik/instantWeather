3593b4c03a92956d352693a7069db6971ff4d736
jean237 <jean237@unicaen.fr>
2024-10-08 15:08:55 +0200

    creation requete api meteo, listener du bouton et affichage technique des infos

--- script/script.js
+++ script/script.js
@@ -2,6 +2,8 @@ const information = document.getElementById("information");
 const codePostal = document.getElementById("cp");
 const erreur = document.getElementById("errorMessage");
 const communeSelect = document.getElementById("communeSelect");
+const boutonSubmitCommune = document.getElementById("submit");
+const divMeteo = document.getElementById("infoMeteo");
 
 codePostal.addEventListener("input", (e) => {
     if (e.target.type === "number" && !e.key.match(/^[0-9]+$/)) {
@@ -31,6 +33,29 @@ codePostal.addEventListener("input", (e) => {
     }
 }, false);
 
+boutonSubmitCommune.addEventListener("click",(e)=>{
+  getDataMeteo().then((json)=>{
+    if (json == -1) {
+      console.log("erreur");
+      erreur.classList.remove("cache");
+  } else {
+    console.log("ça passe");
+      erreur.classList.add("cache");
+      const balisePTemperatureMin = document.createElement('p').textContent = json["tmin"];
+      const balisePTemperatureMax = document.createElement('p').textContent =  json["tmax"];
+      const balisePProbaPluie = document.createElement('p').textContent = json["probarain"];
+      const balisePHeureSoleil = document.createElement('p').textContent = json["sun_hours"];
+      divMeteo.append(balisePTemperatureMin);
+      divMeteo.append(balisePTemperatureMax);
+      divMeteo.append(balisePProbaPluie);
+      divMeteo.append(balisePHeureSoleil);
+
+  }
+})
+})
+
+
+
 async function getData() {
     const url = "https://geo.api.gouv.fr/communes?codePostal=" + codePostal.value;
     try {
@@ -45,3 +70,21 @@ async function getData() {
         return -1;
     }
 }
+
+async function getDataMeteo() {
+  const url = "https://api.meteo-concept.com/api/forecast/daily?token=75e38b3c4f84616e8c5d703a6a6271ffcdad018bac865662eeb3f8b5766bec42&insee=35238";
+  console.log("datameteo")
+  try {
+      const response = await fetch(url);
+      if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+      }
+      var json = await response.json();
+      let reponse = json["forecast"][0]; // meteo sur 1 jour
+      console.log(reponse);
+      return reponse;
+  } catch (error) {
+      return -1;
+  }
+}
+
